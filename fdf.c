5d0dc32ac3f61c7916742d9a0a169dbb2c174680
spookier <acostin444@gmail.com>
2023-01-27 14:54:14 +0100

    window buttons

--- fdf.c
+++ fdf.c
@@ -1,50 +1,68 @@
 #include "fdf.h"
 
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+void    my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
-	char	*dst;
+    char    *dst;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+    dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+    *(unsigned int*)dst = color;
 }
 
 
-void draw_line(t_data *data, t_coordinates *pos, int color)
+int	keys(int key, t_data *mlx)
 {
-	int i = 0;
-	while(i < pos->x1)
+	if (key == ESC)
 	{
-		my_mlx_pixel_put(data, pos->x, pos->y, color);
-		pos->x = pos->x + 1;
-		i++;
+		exit(0);
+		return (0);
+	}
+	return (0);
+}
+
+int	ft_exit(void)
+{
+	exit(1);
+	return (0);
+}
+
+int    main(void)
+{
+    t_data    mlx;
+
+    mlx.mlx = mlx_init();
+    mlx.mlx_win = mlx_new_window(mlx.mlx, 1080, 1080, "Hello world!");
+    mlx.img = mlx_new_image(mlx.mlx, 1080, 1080);
+    mlx.addr = mlx_get_data_addr(mlx.img, &mlx.bits_per_pixel, &mlx.line_length,
+                                &mlx.endian);
+
+
+	int y_start = 400;
+	int y_end = 800;
+	int x_start = 200;
+	int x_end = 500;
+
+	while(y_start < y_end)
+	{
+		x_start = 200;
+		while(x_start < x_end)
+		{
+			my_mlx_pixel_put(&mlx.mlx, x_start, y_start, 0x00FFFFFF);
+			x_start++;
+		}
+    	y_start++;    
 	}
 
+
+    mlx_put_image_to_window(mlx.mlx, mlx.mlx_win, mlx.img, 0, 0);
+	mlx_hook(mlx.mlx_win, 17, 2, ft_exit, (void *)0);
+	mlx_key_hook(mlx.mlx_win, keys, (void *)&mlx);
+    mlx_loop(mlx.mlx);
+
+	
 }
 
 
-int	main(void)
-{
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
-
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1080, 1080, "Hello world!");
-	img.img = mlx_new_image(mlx, 1080, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
-
-	//my_mlx_pixel_put(&img, 5, 5, 0x00FF0000)
-
-	t_coordinates pos_line = {100, 800, 1080/2, 1080/2};
-	draw_line(&img, &pos_line, 0xFFFFFF);
-
-
-
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
-}
 
 
 // cc main.c -Lmlx_linux -lmlx_Linux -L/usr/lib -Imlx_Linux -lXext -lX11 -lm && ./a.out
--- fdf.h
+++ fdf.h
@@ -1,32 +1,21 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include "stdlib.h"
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
 #include "mlx_linux/mlx.h"
-#include "math.h"
 
-#include "stdio.h"
-
-
-typedef struct s_coordinates {
-
-	int x;
-	int x1;
-	int y;
-	int y1;	
-
-}	t_coordinates;
-
+# define ESC		0xFF1B
 
 typedef struct	s_data {
+	void    *mlx;
+    void    *mlx_win;
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
 	int		line_length;
 	int		endian;
 }				t_data;
-
-
-
 
 #endif
